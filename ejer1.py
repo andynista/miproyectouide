ea984c6b9fc3c7b5cb68fe2c81d9611064cf84a6
unknown <cachangosu@uide.edu.ec>
2025-11-22 21:45:29 -0500

    agregue el ejer1.html

---
+++ ejer1.py
@@ -0,0 +1,12 @@
+var="hola,este es mi primer programa"
+print("Bienvenidos")
+print(var)
+x=5
+y=20
+resultado=x+y
+print("resultado de la suma es:",resultado)
+var1=int(input("ingresa un valor: "))
+var2=int(input("ingresa el segundo valor  un valor:"))
+#suma=int(var1)+int(var2)
+suma=var1+var2
+print("el resultado de la suma:",suma)
